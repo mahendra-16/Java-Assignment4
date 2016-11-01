502462baee22bb421a0cfb6f5ff9c407012b8498
mahendra-16 <prajapatimahendra@ymail.com>
2016-11-01 20:29:16 +0530

    look in

---
+++ Assignment4.java
@@ -0,0 +1,22 @@
+
+public class Assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int x = 5;
+		int y = 10;
+		int expression1 = x + y * 2;
+		int expression2 = x - y + 2;
+		int expression3 = (x + y) * 2;
+		int expression4 = y % x ;
+		
+		System.out.println("Expression1 : " + expression1);
+		System.out.println("Expression2 : " + expression2);
+		System.out.println("Expression3 : " + expression3);
+		System.out.println("Expression4 : " + expression4);
+		
+		
+	}
+
+}
